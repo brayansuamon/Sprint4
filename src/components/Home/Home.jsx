97b0f97889c15cebc4f67896095b41edcdba4be9
brayansuamon <brayansuamon@gmail.com>
2022-02-02 10:53:29 -0500

    Add component Header and Home

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Home.module.scss";
+
+export default function Home(params) {
+  return (
+    <section>
+      <figure>
+        <img src="./images/logo_devs.svg" alt="Devs" />
+      </figure>
+    </section>
+  );
+}
---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_LJnrP6ZAxrcoyv0lU9dSRGUGwlnewrw",
+  authDomain: "users-d9f6d.firebaseapp.com",
+  projectId: "users-d9f6d",
+  storageBucket: "users-d9f6d.appspot.com",
+  messagingSenderId: "363465164939",
+  appId: "1:363465164939:web:f89b554c1de7784aa3ac8e",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
