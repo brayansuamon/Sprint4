feecd2630fadd64e60c3509db8423564a6c5b1a9
brayansuamon <brayansuamon@gmail.com>
2022-02-03 14:53:37 -0500

    Added HTML-Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,10 +3,18 @@ import styles from "./Home.module.scss";
 
 export default function Home(params) {
   return (
-    <section>
-      <figure>
+    <section className={styles.home}>
+      <figure className={styles.image}>
         <img src="./images/logo_devs.svg" alt="Devs" />
       </figure>
+      <form className={styles.signIn}>
+        <h1>LOREM IPSUM DOLOR</h1>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+        <figure className={styles.google}>
+          <img src="./images/Google.svg" alt="Google" />
+          <button>Sign in with Google</button>
+        </figure>
+      </form>
     </section>
   );
 }
