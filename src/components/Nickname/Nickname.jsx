ebb4bb12daa4720629618d615031b951db09c9a1
brayansuamon <brayansuamon@gmail.com>
2022-02-10 11:53:21 -0500

    Added NickName HTML and CSS Styles

--- src/components/Nickname/Nickname.jsx
+++ src/components/Nickname/Nickname.jsx
@@ -9,12 +9,12 @@ function Nickname(params) {
         <img src="./images/logo_devs.svg" alt="Devs" />
       </figure>
       <form className={styles.signIn}>
-        <h1>LOREM IPSUM DOLOR</h1>
-        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
-        <button onClick={login} className={styles.google}>
-          <img src="./images/Google.svg" alt="Google" />
-          <div>Sign in with Google</div>
-        </button>
+        <h1>
+          WELCOME <span>Name!</span>
+        </h1>
+        <input type="text" placeholder="Type your username" />
+        <h3>Select your favorite color</h3>
+        <button>CONTINUE</button>
       </form>
     </section>
   );
