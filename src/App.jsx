4dc4fe4d3ded77b84fd5e12038ab99b04218752c
brayansuamon <brayansuamon@gmail.com>
2022-02-10 10:19:01 -0500

    Commented APP section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { React } from "react";
-import Home from "./components/Home/Home";
 import "./App.css";
-import Profile from "./components/Profile/Profile";
-import PostMessage from "./components/PostMessage/PostMessage";
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
-      <Profile />
-      <PostMessage />
+      {/* <Profile /> */}
+      {/* <PostMessage /> */}
     </div>
   );
 }
