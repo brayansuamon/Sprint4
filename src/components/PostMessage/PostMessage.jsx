65bc307f1c94e1a021d66344827eb47bef42d773
brayansuamon <brayansuamon@gmail.com>
2022-02-19 16:39:36 -0500

    Adjust Post_Message section

--- src/components/PostMessage/PostMessage.jsx
+++ src/components/PostMessage/PostMessage.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import styles from "./PostMessage.module.scss";
-
+import { AppContext } from "../../context/AppContext";
 const { body, header } = styles;
 
 function PostMessage(params) {
+  const { state } = useContext(AppContext);
+
+  //To catch the tweets in the text area
+  const [tweet, setTweet] = useState("");
+
+  function handletext(e) {
+    setTweet(e.target.value);
+  }
+
   return (
     <div className={body}>
       <header className={header}>
-        <img src="./images/image_logo_devs.png" alt="Username" />
+        <img
+          className={styles.userphoto}
+          src={state.userData.photo}
+          alt="Username"
+        />
         <img src="./images/image_logo_devs.png" alt="Logo_devs" />
         <img
           className={`${styles.textdev} ${styles.algo}`}
@@ -17,7 +30,7 @@ function PostMessage(params) {
       </header>
       <section className={styles.write_message}>
         <picture className={styles.user_message}>
-          <img src="./images/image_logo_devs.png" alt="" />
+          <img src={state.userData.photo} alt="username" />
         </picture>
         <section className={styles.section_message}>
           <textarea
@@ -25,10 +38,12 @@ function PostMessage(params) {
             id=""
             maxLength={200}
             placeholder="Type Something"
+            value={tweet}
+            onChange={handletext}
           ></textarea>
         </section>
         <aside className={styles.characters_message}>
-          <p className={styles.num_characters}>NumberCh</p>
+          <p className={styles.num_characters}>{tweet.length}</p>
           <p className={styles.max_characters}>Max 200.</p>
         </aside>
         <aside className={styles.button_message}>
